c850af184e13772927a92000abcef0b9adb32d53
Faris Redza <faris.redza1999@gmail.com>
2024-05-08 14:59:55 +0100

    first working compile

--- setup.py
+++ setup.py
@@ -1,5 +1,19 @@
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 import numpy as np
+
+
+tomtag_module = Extension(
+    "tomtag",
+    sources=[
+        'src/tomtag/correlation_module.c',
+        'src/tomtag/correlation.c',
+    ],
+    include_dirs=[
+        np.get_include(),
+        'src/tomtag'
+    ]
+)
+
 
 def main():
     setup(
@@ -7,27 +21,16 @@ def main():
         version='0.11',
         description='time tag correlation library ',
         long_description='This package holds several functions to process raw timetags that would be output by a tagger like e.g. a UQD Logic16.',
-        author='Faris Redza',
-        author_email='faris.redza1999@gmail.com',
-        url='https://github.com/FarisRedza/tomtag',
+        author='Thomas Jaeken',
+        author_email='tj2022@hw.ac.uk',
+        url='https://github.com/Thomas-Jaeken/tomtag',
         requires=['numpy'],
-        ext_modules=[
-            Extension(
-                name='tomtag',
-                sources=[
-                    np.get_include(),
-                    'src/tomtag/correlation_module.c',
-                    'src/tomtag/correlation.c',
-                    'src/tomtag/correlation.h',
-                ],
-            ),
-        ],
+        ext_modules=[tomtag_module],
         classifiers=[
             'Programming Language :: Python :: 3',
             'License :: OSI Approved :: MIT License',
             'Operating System :: OS Independent',
         ],
-        include_dirs='src/tomtag'
     )
 
 if __name__ == '__main__':

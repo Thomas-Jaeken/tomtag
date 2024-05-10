87fa72e8722647d60b477aa4ff58603bdaa6ae23
FarisRedza <faris.redza1999@gmail.com>
2024-05-10 15:37:24 +0100

    fixed build process

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, Extension
+import numpy as np
+
+setup_args = dict(
+    ext_modules = [
+        Extension(
+            name='tomtag',
+            sources=[
+                'src/tomtag/correlation.c',
+                'src/tomtag/correlation_module.c',
+            ],
+            include_dirs=[
+                np.get_include(),
+                'src/tomtag'
+            ],
+        )
+    ],
+)
+setup(**setup_args)
